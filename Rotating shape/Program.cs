42f5d7efbb8f7c8c366bd545eb2b69052c2147b0
Students <Students@DESKTOP-DAPA1OB>
2025-06-10 21:40:23 -1100

    WOrks Wow also study matirces

--- Rotating shape/Program.cs
+++ Rotating shape/Program.cs
@@ -1,191 +1,127 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.Numerics;
 
 namespace Rotating_shape
 {
     internal class Program
     {
-
         class point
         {
-            public double x = 0;
-            public double y = 0;
+            public Vector3 position;
+            public point(float x, float y, float z)
+            {
+                position = new Vector3(x, y, z);
+            }
 
-            public point(double x, double y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-            public point(double x, double y,int size)
-            {
-                this.x = x*size;
-                this.y = y*size;
-            }
         }
 
         class shape
         {
-            public point a, b, c, d;
-            public int x, y;
-            public int length = 1;
+            public List<point> points = new List<point>();
 
-            public shape(int size)
+            public void addPoint(float x, float y, float z)
             {
-                
-                int s = size;
-                
-                a = new point(-1, -1, s);
-                b = new point(0.5, -0.5, s);
-                c = new point(1, 1, s);
-                d = new point(-0.5, 0.5, s);
+                points.Add(new point(x, y, z));
             }
-        }
-        class Polygon
-        {
-            public List<point> Points= new List<point>();
-            public int x, y;
-            public int length = 1;
 
-            public Polygon(int size)
+
+
+            public void Rotate(float angleX, float angleY, float angleZ)
             {
+                Matrix4x4 rotationX = Matrix4x4.CreateRotationX(angleX);
+                Matrix4x4 rotationY = Matrix4x4.CreateRotationY(angleY);
+                Matrix4x4 rotationZ = Matrix4x4.CreateRotationZ(angleZ);
 
-                int s = size;
-
-                Points.Add( new point(-1, -1, s));
-                Points.Add(new point(1, -1, s));
-                Points.Add(new point(1, 1, s));
-                Points.Add(new point(-1, 2, s));
+                foreach (point p in points)
+                {
+                    Vector3 rotatedPosition = Vector3.Transform(p.position, rotationX);
+                    rotatedPosition = Vector3.Transform(rotatedPosition, rotationY);
+                    rotatedPosition = Vector3.Transform(rotatedPosition, rotationZ);
+                    p.position = rotatedPosition;
+                }
             }
+
+            public void drawLine(int B,int A,Vector2 Offset)
+            {
+                int a = A;
+                int b = B;
+                DrawLine((int)(points[a].position.X + Offset.X), (int)(points[a].position.Y + Offset.Y), (int)(points[b].position.X + Offset.X), (int)(points[b].position.Y + Offset.Y));
+
+            }
+
+
         }
 
 
-        static void Rotate(float angle, shape shape)
-        {
-            float radians = angle * (float)(Math.PI / 180.0);
-            //Matrix for rotation
-            float cos = (float)Math.Cos(radians);
-            float sin = (float)Math.Sin(radians);
-            // Rotate each point
-            point tempA = new point(shape.a.x, shape.a.y);
-            point tempB = new point(shape.b.x, shape.b.y);
-            point tempC = new point(shape.c.x, shape.c.y);
-            point tempD = new point(shape.d.x, shape.d.y);
 
-            tempA.x = shape.a.x * cos - shape.a.y * sin;
-            tempA.y = shape.a.x * sin + shape.a.y * cos;
-            tempB.x = shape.b.x * cos - shape.b.y * sin;
-            tempB.y = shape.b.x * sin + shape.b.y * cos;
-            tempC.x = shape.c.x * cos - shape.c.y * sin;
-            tempC.y = shape.c.x * sin + shape.c.y * cos;
-            tempD.x = shape.d.x * cos - shape.d.y * sin;
-            tempD.y = shape.d.x * sin + shape.d.y * cos;
-
-            // Update the shape points
-            shape.a = tempA;
-            shape.b = tempB;
-            shape.c = tempC;
-                shape.d = tempD;
-
-            
-
-        }
         static void Main(string[] args)
         {
-            float time = 0;
-            Thread deltatime = new Thread(() =>
-            {
-                while (true)
-                {
-                    Thread.Sleep(1);
-                    time += 0.001f; // Increment time by 1 millisecond
-                }
-            });
-            Console.ForegroundColor = ConsoleColor.Blue;
+            int lenght  = 20;
+            shape cube = new shape();
+            cube.addPoint(-lenght, -lenght, -lenght);
+            cube.addPoint(lenght, -lenght, -lenght);
+            cube.addPoint(lenght, lenght, -lenght);
+            cube.addPoint(-lenght, lenght, -lenght);
+            cube.addPoint(-lenght, -lenght, lenght);
+            cube.addPoint(lenght, -lenght, lenght);
+            cube.addPoint(lenght, lenght, lenght);
+            cube.addPoint(-lenght, lenght, lenght);
 
-            shape square = new shape(21);
-            shape square2 = new shape(17);
-            square.x = 70;
-            square.y = 60;
-            square2.x = 70;
-            square2.y = 60;
-            shape square3 = new shape(10);
-            shape square4 = new shape(13);
-            square3.x = 70;
-            square3.y = 60;
-            square4.x = 70;
-            square4.y = 60;
-            Console.ReadLine();
-            float speed = 0f;
-            var input = Console.ReadKey().Key;
+            Vector2 Offset = new Vector2(lenght*2, lenght*2);
             while (true)
             {
                 
-                if (Console.KeyAvailable)
+                cube.Rotate(0.01f, 0.1f, 0.1f);
+                foreach (point p in cube.points)
                 {
-                    input = Console.ReadKey(true).Key;
-                }
+                    float z = p.position.Z / 2;
+                    WriteAt(p.position.X + Offset.X, p.position.Y + Offset.Y);
 
 
-                Rotate(speed, square);
-                if (input == ConsoleKey.E)
-                {
-                    speed += 0.1f;
-                    
+                                                                                                                                   
                 }
-                else if (input == ConsoleKey.Q)
-                {
-                    speed -= 0.1f;
-                }
-                else
-                {
-                    if (speed > 0)
-                    {
-                        speed -= 0.1f;
-                    }
-                    else if(speed < 0)
-                    {
-                        speed += 0.1f;
-                    }
-                }
-                input = ConsoleKey.None;
+                
+                cube.drawLine(0, 1, Offset);
+                cube.drawLine(1, 2, Offset);
+                cube.drawLine(2, 3, Offset);
+                cube.drawLine(3, 0, Offset);
+                cube.drawLine(4, 5, Offset);
+                cube.drawLine(5, 6, Offset);
+                cube.drawLine(6, 7, Offset);
+                cube.drawLine(7, 4, Offset);
+                cube.drawLine(0, 4, Offset);
+                cube.drawLine(1, 5, Offset);
+                cube.drawLine(2, 6, Offset);
+                cube.drawLine(3, 7, Offset);
+
+
+
+
+
+
+
+
                 Thread.Sleep(10);
+                //Console.ReadLine();
                 Console.Clear();
-                
-               
-                WriteAt("██", square);
-                
-
-
-
             }
 
 
+
         }
 
-        static void WriteAt(string text, int x, int y)
+        static void WriteAt(float x, float y)
         {
-
-            Console.SetCursorPosition(x, y);
-            Console.Write(text);
-        }
-        static void WriteAt(string text, shape shape)
-        {
-            int lenght = shape.length;
-            WriteAt(text, 2*((int)shape.a.x + shape.x), (int)shape.a.y+shape.y);
-
-            WriteAt(text, 2*((int)shape.b.x + shape.x), (int)shape.b.y+shape.y);
-            
-            WriteAt(text, 2*((int)shape.c.x + shape.x), (int)shape.c.y+shape.y);
-            WriteAt(text, 2*((int)shape.d.x + shape.x), (int)shape.d.y+shape.y);
-            drawLine(((int)shape.a.x + shape.x), (int)shape.a.y + shape.y,((int)shape.b.x + shape.x), (int)shape.b.y + shape.y);
-            drawLine(((int)shape.b.x + shape.x), (int)shape.b.y + shape.y, ((int)shape.c.x + shape.x), (int)shape.c.y + shape.y);
-            drawLine(((int)shape.c.x + shape.x), (int)shape.c.y + shape.y, ((int)shape.d.x + shape.x), (int)shape.d.y + shape.y);
-            drawLine(((int)shape.d.x + shape.x), (int)shape.d.y + shape.y, ((int)shape.a.x + shape.x), (int)shape.a.y + shape.y);
-
+            try
+            {
+                Console.SetCursorPosition((int)x * 2, (int)y);
+                Console.Write("██");
+            }
+            catch { }
 
         }
 
-        static void drawLine(int x1, int y1, int x2, int y2)
+
+        static void DrawLine(int x1, int y1, int x2, int y2)
         {
             int dx = x2 - x1;
             int dy = y2 - y1;
@@ -198,10 +134,13 @@ namespace Rotating_shape
 
             for (int i = 0; i <= steps; i++)
             {
-                WriteAt("██", (int)x*2, (int)y);
+                WriteAt((int)x, (int)y);
                 x += xIncrement;
                 y += yIncrement;
             }
         }
+
+        
+
     }
 }
